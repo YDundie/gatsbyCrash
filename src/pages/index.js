1f28393dc8214289f7edd9017cba5a24ae944ab2
YDundie <mustacmarko@yahoo.com>
2021-09-06 19:15:57 +0200

    Gatsby plugins, graphql, images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,36 @@
 import React from "react"
 import Layout from '../components/Layout'
 import * as homeStyles from '../styles/home.module.css'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 
-export default function Home() {
+export default function Home({ data }) {
+  const image = data.file.childImageSharp.fluid
   return (
     <Layout>
       <section className={homeStyles.header}>
         <div>
-          <h2>Design</h2>
           <h3>Devlop & Deploy</h3>
           <p>UX designer & web developer based in Sveta Marija.</p>
           <Link className={homeStyles.btn} to='/projects'>My Portfolio Projects</Link>
         </div>
+        <Img fluid={image} />
       </section>
     </Layout>
   )
 }
+
+
+export const query = graphql`
+query banner {
+  file(relativePath: {eq: "banner.png"}) {
+    id
+    childImageSharp {
+      fluid {
+       ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,60 @@
 import React from 'react'
 import Layout from '../../components/Layout';
 import * as projectStyles from '../../styles/projects.module.css'
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image'
 
-export default function Projects() {
+export default function Projects({ data }) {
+    console.log('data', data)
+    const projects = data.projects.nodes
+    const contact = data.contact.siteMetadata.contact
     return (
         <Layout>
             <div className={projectStyles.portfolio}>
                 <h2>Projects</h2>
                 <h3>Project & Websites I've created.</h3>
+                <div className={projectStyles.projects}>
+                    {projects.map(project => (
+                        <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+                            <div>
+                                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid}></Img>
+                                <h3>{project.frontmatter.title}</h3>
+                                <p>{project.frontmatter.stack}</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+                <p>Like what you see? <strong>  Contact me at {contact} </strong></p>
             </div>
         </Layout>
     )
 }
+
+
+export const query = graphql`
+query ProjectsPage {
+    projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          stack
+          title
+          slug
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+  
+`
